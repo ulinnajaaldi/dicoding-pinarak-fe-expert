9cb9f1987b00255d6f39132d293e2fdb78a81671
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-05-07 20:54:56 +0700

    fix: all function validation

--- submission-1/src/scripts/view/main.js
+++ submission-1/src/scripts/view/main.js
@@ -1,34 +1,58 @@
 const main = () => {
-  const hamburgerButtonElement = document.querySelector("#hamburger");
-  const navListElement = document.querySelector(".nav_list");
+  const hamburgerButton = document.querySelector("#hamburger");
+  const navList = document.querySelector(".nav_list");
+  const navItems = document.querySelectorAll(".nav_item");
   const mainElement = document.querySelector("main");
-  const headerElement = document.querySelector("header");
+  const header = document.querySelector("header");
+  const skipLink = document.querySelector(".skip_link");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 0) {
-      headerElement.classList.add("scrolled");
-    } else {
-      headerElement.classList.remove("scrolled");
+    header.classList.toggle("scrolled", window.scrollY > 0);
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Tab" && navList.classList.contains("open")) {
+      const lastNavItem = navItems[navItems.length - 1];
+      if (lastNavItem === event.target) {
+        navList.classList.remove("open");
+        mainElement.classList.remove("blur");
+        hamburgerButton.innerHTML = `&#9776;`;
+        hamburgerButton.ariaLabel = "Buka menu navigasi";
+      }
     }
   });
 
-  hamburgerButtonElement.addEventListener("click", (event) => {
-    navListElement.classList.toggle("open");
-    if (navListElement.classList.contains("open")) {
-      hamburgerButtonElement.innerHTML = `&times;`;
-      hamburgerButtonElement.ariaLabel = "Tutup menu navigasi";
-    } else {
-      hamburgerButtonElement.innerHTML = `&#9776;`;
-      hamburgerButtonElement.ariaLabel = "Buka menu navigasi";
-    }
-
+  hamburgerButton.addEventListener("click", (event) => {
+    navList.classList.toggle("open");
+    hamburgerButton.innerHTML = navList.classList.contains("open")
+      ? "&times"
+      : "&#9776";
+    hamburgerButton.ariaLabel = navList.classList.contains("open")
+      ? "Tutup menu navigasi"
+      : "Buka menu navigasi";
     mainElement.classList.toggle("blur");
     event.stopPropagation();
   });
 
-  mainElement.addEventListener("click", () => {
-    navListElement.classList.remove("open");
+  const closeNavList = () => {
+    navList.classList.remove("open");
     mainElement.classList.remove("blur");
+    hamburgerButton.innerHTML = "&#9776";
+    hamburgerButton.ariaLabel = "Buka menu navigasi";
+  };
+
+  navList.addEventListener("click", closeNavList);
+  mainElement.addEventListener("click", closeNavList);
+
+  const closeSkipLink = () => {
+    skipLink.blur();
+  };
+  skipLink.addEventListener("click", closeSkipLink);
+  skipLink.addEventListener("keydown", (event) => {
+    if (event.key === " ") {
+      event.preventDefault();
+      closeSkipLink();
+    }
   });
 };
 
