3b048d677cafdd84dc3c6366fc8b72a0e00db222
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-05-07 20:52:10 +0700

    feat: create card for restouran items

---
+++ submission-1/src/scripts/components/card/card-item.js
@@ -0,0 +1,35 @@
+class CardItem extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <div class="resto_item">
+        <div class="resto_image_wrapper">
+          <img
+            src="${this.getAttribute("image")}"
+            alt="Gambar ${this.getAttribute("name")}"
+          />
+          <p>${this.getAttribute("city")}</p>
+        </div>
+        <div class="resto_content">
+          <div class="title">
+            <h1>
+              <a href="#">${this.getAttribute("name")}</a>
+            </h1>
+            <div class="rating" title="Rating ${this.getAttribute("name")}">
+              <p>&#9733;</p>
+              <p>${this.getAttribute("rating")}</p>
+            </div>
+          </div>
+          <p class="desc">
+            ${this.getAttribute("description")}
+          </p>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define("card-item", CardItem);
