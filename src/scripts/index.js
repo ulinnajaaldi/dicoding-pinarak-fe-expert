7184ad7a511c81b21b1dbadcf6675aa014e34fea
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-05-18 16:29:24 +0700

    refactor: clear submission 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import "regenerator-runtime"; /* for async await transpile */
-import "../styles/style.scss";
+import "../styles/index.scss";
+import "../styles/responsive.scss";
 
 import main from "./view/main";
 import "./components/item-list";
