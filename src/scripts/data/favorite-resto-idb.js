78970884444ce9467c60b01e143a467dcedb4f26
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-09 21:50:15 +0700

    feat: creating IndexDB configuration for Favorite item

---
+++ src/scripts/data/favorite-resto-idb.js
@@ -0,0 +1,36 @@
+/* eslint-disable consistent-return */
+import { openDB } from 'idb';
+import Config from '../utils/config';
+
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = Config;
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+  },
+});
+const FavoriteRestaurantIdb = {
+  async getRestaurant(id) {
+    if (!id) {
+      return;
+    }
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+
+  async getAllRestaurant() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+
+  async putRestaurant(restaurant) {
+    if (!restaurant.hasOwnProperty('id')) {
+      return;
+    }
+    return (await dbPromise).put(OBJECT_STORE_NAME, restaurant);
+  },
+
+  async deleteRestaurant(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavoriteRestaurantIdb;
