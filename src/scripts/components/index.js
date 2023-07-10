90de0f2f6b6d7e4158145b194435ca2269d0c88a
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-11 00:08:11 +0700

    feat: adding loader components

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -2,3 +2,5 @@ import './navbar-element';
 import './footer-element';
 import './card/card-item';
 import './card/card-search-item';
+import './loader/loading-circle';
+import './loader/loading-row';
